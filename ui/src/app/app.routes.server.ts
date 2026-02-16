73bcba597e68192a500d875ff397c84377af3a0b
viki1003 <viki8062@gmail.com>
2026-02-16 13:15:44 +0530

    Fix: ensure index.html is generated via prerendering static routes

--- ui/src/app/app.routes.server.ts
+++ ui/src/app/app.routes.server.ts
@@ -2,6 +2,26 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'login',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'register',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'admin',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'movie/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Server
   }
