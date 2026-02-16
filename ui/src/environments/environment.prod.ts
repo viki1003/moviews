826026bca3ded9271384f0b22aa0a98e1916c118
viki1003 <viki8062@gmail.com>
2026-02-16 13:34:10 +0530

    Update: new tunnel URL and security settings

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'https://api.moviews.com' // Replace with your EC2 public IP or domain
+    apiBaseUrl: 'https://excellent-statutory-soc-induced.trycloudflare.com' // Replace with your EC2 public IP or domain
 };
